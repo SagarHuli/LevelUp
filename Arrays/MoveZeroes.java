c6dc2ac11e0306a0ecdaa054018d70427efe5e87
Sagar Huli <sagarhuli72@gmail.com>
2025-06-17 07:27:25 +0530

    P1

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,21 @@
+package LevelUp.Arrays;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        if(nums.length >1){
+            int zeroes = 0;
+            int j=0;
+            for(int i=0;i<len;i++) {
+                if(nums[i]==0)
+                    zeroes++;
+                else {
+                    nums[j++] = nums[i];
+                }
+            }
+            while(zeroes-- >0) {
+                nums[j++] = 0;
+            }
+        }
+    }
+}
