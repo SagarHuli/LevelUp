6554b7d070ce7ca2e1735c036a2a75e043eeb9e8
Sagar Huli <sagarhuli72@gmail.com>
2025-07-03 21:32:30 +0530

    P36

---
+++ HashTable/DesignHashMap.java
@@ -0,0 +1,34 @@
+package HashTable;
+
+public class DesignHashMap {
+}
+
+class MyHashMap {
+
+    int [][] ar;
+    public MyHashMap() {
+        this.ar = new int[1000001][2];
+    }
+
+    public void put(int key, int value) {
+        this.ar[key][0] = value;
+        this.ar[key][1] = 1;
+    }
+
+    public int get(int key) {
+        return this.ar[key][1] ==0? -1 : this.ar[key][0];
+    }
+
+    public void remove(int key) {
+        this.ar[key][0] = -1;
+        this.ar[key][1] = 0;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
