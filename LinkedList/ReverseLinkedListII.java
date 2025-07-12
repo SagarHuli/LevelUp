a872420f1861839a2b39031dce1d3a6b6bd0e3d3
Sagar Huli <sagarhuli72@gmail.com>
2025-07-12 12:15:20 +0530

    P58

---
+++ LinkedList/ReverseLinkedListII.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+public class ReverseLinkedListII {
+    /*
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || right-left==0)
+            return head;
+        ListNode curr = head;
+        ListNode prev1= null, prev2= null;
+        Stack<Integer> stack = new Stack<>();
+        int size =1;
+        while(curr!=null) {
+            if(size>=left && size <= right) {
+                stack.push(curr.val);
+            } curr = curr.next;
+            size++;
+        }
+        curr = head;
+        size = 1;
+        while(curr!=null) {
+            if(size>=left && size <= right){
+                curr.val = stack.pop();
+            } curr = curr.next;
+            size++;
+        }
+        return head;
+    }
+    */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || left==right) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode leftPrev = dummy;
+        ListNode curr = head;
+        for(int i=0;i<left-1;i++) {
+            leftPrev = curr;
+            curr = curr.next;
+        }
+        ListNode prev = null;
+        for(int i=0;i<right-left+1;i++) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        leftPrev.next.next = curr;
+        leftPrev.next = prev;
+
+        return dummy.next;
+    }
+}
