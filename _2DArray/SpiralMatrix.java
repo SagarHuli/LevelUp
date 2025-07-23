eeedfeadad329816e4157486614ea498fe51e1f8
Sagar Huli <sagarhuli72@gmail.com>
2025-07-23 07:42:51 +0530

    P82

---
+++ _2DArray/SpiralMatrix.java
@@ -0,0 +1,34 @@
+package _2DArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int iTop=0,iBottom=matrix.length-1,jLeft=0,jRight=matrix[0].length-1;
+        List<Integer> list = new ArrayList<>();
+        while(iTop<=iBottom && jLeft<=jRight) {
+            for(int i=jLeft;i<=jRight;i++) {
+                list.add(matrix[iTop][i]);
+            }
+            iTop++;
+            for(int i=iTop;i<=iBottom;i++) {
+                list.add(matrix[i][jRight]);
+            }
+            jRight--;
+            if(iTop<=iBottom) {
+                for(int i=jRight;i>=jLeft;i--) {
+                    list.add(matrix[iBottom][i]);
+                }
+                iBottom--;
+            }
+            if(jLeft<=jRight) {
+                for(int i=iBottom;i>=iTop;i--) {
+                    list.add(matrix[i][jLeft]);
+                }
+                jLeft++;
+            }
+        }
+        return list;
+    }
+}
