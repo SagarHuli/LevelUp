e9d8d3abc649791d932556309cbdd7c7cdab2cde
Sagar Huli <sagarhuli72@gmail.com>
2025-12-17 18:15:27 +0530

    P185

---
+++ Maths_Geometry/ValidSquare.java
@@ -0,0 +1,26 @@
+package Maths_Geometry;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSquare {
+
+    /*
+    https://leetcode.com/problems/valid-square/submissions/1858030915/
+     */
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        Set<Integer> set = new HashSet<>();
+        set.add(distance(p1,p2));
+        set.add(distance(p1,p3));
+        set.add(distance(p1,p4));
+        set.add(distance(p2,p3));
+        set.add(distance(p2,p4));
+        set.add(distance(p3,p4));
+
+        return set.size() == 2 && !set.contains(0);
+    }
+
+    private int distance(int[] p1, int[] p2) {
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
+    }
+}
